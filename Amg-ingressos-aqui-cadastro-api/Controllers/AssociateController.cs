ae67e92e4eaa7780229dd90e33cfd09bf02a9f42
Leonardo Felix <leonardofelix2004@hotmail.com>
2023-12-03 10:34:15 -0300

    Retirado validacao cadastro user colab e adicionado no vinculo com organizador

--- Amg-ingressos-aqui-cadastro-api/Controllers/AssociateController.cs
+++ Amg-ingressos-aqui-cadastro-api/Controllers/AssociateController.cs
@@ -3,8 +3,10 @@ using Amg_ingressos_aqui_cadastro_api.Dtos;
 using Amg_ingressos_aqui_cadastro_api.Enum;
 using Amg_ingressos_aqui_cadastro_api.Exceptions;
 using Amg_ingressos_aqui_cadastro_api.Model;
+using Amg_ingressos_aqui_cadastro_api.Repository.Interfaces;
 using Amg_ingressos_aqui_cadastro_api.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Amg_ingressos_aqui_cadastro_api.Controllers
 {
@@ -15,12 +17,21 @@ namespace Amg_ingressos_aqui_cadastro_api.Controllers
         private readonly ILogger<AssociateController> _logger;
         private readonly IAssociateService _associateService;
         private readonly IUserService _userService;
+        private readonly IAssociateColabOrganizerRepository _organizerRepository;
+        private readonly IUserRepository _userRepository;
 
-        public AssociateController(ILogger<AssociateController> logger, IAssociateService associateService, IUserService userService)
+        public AssociateController(
+            ILogger<AssociateController> logger, 
+            IAssociateService associateService, 
+            IUserService userService, 
+            IUserRepository userRepository,
+            IAssociateColabOrganizerRepository organizerRepository)
         {
             _logger = logger;
             _associateService = associateService;
             _userService = userService;
+            _userRepository = userRepository;
+            _organizerRepository = organizerRepository;
         }
 
         /// <summary>
@@ -33,18 +44,80 @@ namespace Amg_ingressos_aqui_cadastro_api.Controllers
         [Route("organizer/{idUserOrganizer}")]
         public async Task<IActionResult> AssociateColabWithOrganizerAsync([FromRoute] string idUserOrganizer,[FromBody] UserDTO user)
         {
+            var idUserCollaborator = string.Empty;
+            User userSave = new User();
             try
             {
+                //Consulta todos os colaboradores vinculados ao Organizador do evento
+                var listAssociate = (List<AssociateCollaboratorOrganizer>)_organizerRepository.FindAllColabsOfProducer<AssociateCollaboratorOrganizer>(idUserOrganizer).Result;
+
+                //Se o Id do user, estiver vazio, consulta se email ou documentId (cpf) já existe para o tipo colaborador
+                if (string.IsNullOrEmpty(user.Id))
+                {
+                    //Consulta se user do email, é colaborador e se já esta vinculado ao organizador do evento
+                    var userData = (UserDTO)_userService.FindByEmailAsync(TypeUserEnum.Collaborator, user.Contact.Email).Result.Data;
+                    if (userData != null && userData.Type == TypeUserEnum.Collaborator && listAssociate.Exists(x => x.IdUserCollaborator == userData.Id))
+                    {
+                        return BadRequest(MessageLogErrors.FindUserAssociateColab);
+                    }
+                    else if(userData == null)
+                    {
+                        //Consulta se user do documentId, é colaborador e se já esta vinculado ao organizador do evento
+                        User userLocal = await _userRepository.FindByField<User>("DocumentId", user.DocumentId);
+                        if (userLocal != null && userLocal.Type == TypeUserEnum.Collaborator && listAssociate.Exists(x => x.IdUserCollaborator == userLocal.Id))
+                        {
+                            return BadRequest(MessageLogErrors.FindUserAssociateColab);
+                        }
+                        else if(userLocal != null)
+                        {
+                            idUserCollaborator = userLocal.Id;
+                        }
+                    }
+                    else
+                    {
+                        idUserCollaborator = userData.Id;
+                    }
+
+                    //Se não encontrar os dados user colaborador, cadastra ele
+                    if (string.IsNullOrEmpty(idUserCollaborator))
+                    {
+                        //Insere o colaborador
+                        var userSaveLocal = await _userService.SaveAsync(user);
+
+                        if (userSaveLocal != null && userSaveLocal.Data != null)
+                        {
+                            userSave = (User)userSaveLocal.Data;
+                        }
+
+                        idUserCollaborator = userSave.Id;
+                    }
+                }
+                else
+                {
+                    idUserCollaborator = user.Id;
+                    
+                    //Consulta se o colaborador ja está vinculado ao organizador do evento
+                    if (listAssociate.Any() && listAssociate.Exists(x => x.IdUserCollaborator == user.Id))
+                    {
+                        return BadRequest(MessageLogErrors.FindUserAssociateColab);
+                    }
+                }
+
+                /*
                 var userData = (UserDTO)_userService.FindByEmailAsync(TypeUserEnum.Collaborator,user.Contact.Email).Result.Data;
                 var idUserCollaborator = string.Empty;
                 if(userData != null){
+                    //Pega o id do colaborador
                     idUserCollaborator = userData.Id;
                 }
                 else{
+                    //Insere o colaborador
                     var userSave = (UserDTO)_userService.SaveAsync(user).Result.Data;
                     idUserCollaborator = userSave.Id;
                 }
+                */
 
+                //Executa o vinculo do organizador de evento ao user colaborador
                 MessageReturn result = await _associateService.AssociateColabOrganizerAsync(idUserOrganizer, idUserCollaborator);
                 if (result.hasRunnedSuccessfully())
                     return Ok(result.Data);
